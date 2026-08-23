3563d517f58764d8672cf6e2d1c709c876ed18eb
nihat7space <nihatoksuz68@gmail.com>
2026-08-23 19:42:08 +0300

    Add Docker Compose setup with PostgreSQL

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from backend.models import Base
 from sqlalchemy.orm import sessionmaker
+import os
+
 
 engine = create_engine(
-    "postgresql+psycopg2://nihatoksuz@localhost:5432/fitness_db"
+    f"postgresql+psycopg2://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 )
 
 Base.metadata.create_all(bind=engine)
@@ -17,4 +19,4 @@ def get_db():
     finally:
         db.close()
 
-        
+    
